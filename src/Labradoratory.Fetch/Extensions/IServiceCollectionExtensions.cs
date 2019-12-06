ca7a44bf736fdad4ab4bf297ad6c996f669f3dd5
Ed Downs <ed@labradoratory.com>
2019-12-06 13:54:07 -0800

    Added helper methods for adding processors to DI

--- src/Labradoratory.Fetch/Extensions/IServiceCollectionExtensions.cs
+++ src/Labradoratory.Fetch/Extensions/IServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Labradoratory.Fetch.DependencyInjection;
 using Labradoratory.Fetch.Processors;
+using Labradoratory.Fetch.Processors.DataPackages;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -20,6 +21,96 @@ namespace Labradoratory.Fetch.Extensions
             serviceCollection.TryAddSingleton<ProcessorPipeline>();
             serviceCollection.TryAddSingleton<IProcessorProvider, DefaultProcessorProvider>();
 
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> adding processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchAddingProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityAddingPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityAddingPackage<TEntity>>, TProcessor>();
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> added processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchAddedProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityAddedPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityAddedPackage<TEntity>>, TProcessor>();
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> updating processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchUpdatingProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityUpdatingPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityUpdatingPackage<TEntity>>, TProcessor>();
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> updated processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchUpdatedProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityUpdatedPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityUpdatedPackage<TEntity>>, TProcessor>();
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> deleting processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchDeletingProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityDeletingPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityDeletingPackage<TEntity>>, TProcessor>();
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds the <typeparamref name="TEntity"/> deleted processor, as transient.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TProcessor">The type of the processor.</typeparam>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <returns>The <paramref name="serviceCollection"/>.</returns>
+        public static IServiceCollection AddFetchDeletedProcessor<TEntity, TProcessor>(this IServiceCollection serviceCollection)
+            where TEntity : Entity
+            where TProcessor : class, IProcessor<EntityDeletedPackage<TEntity>>
+        {
+            serviceCollection.AddTransient<IProcessor<EntityDeletedPackage<TEntity>>, TProcessor>();
             return serviceCollection;
         }
     }
--- test/Labradoratory.Fetch.Test/Extensions/IServiceCollectionExtensions_Tests.cs
+++ test/Labradoratory.Fetch.Test/Extensions/IServiceCollectionExtensions_Tests.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Labradoratory.Fetch.DependencyInjection;
 using Labradoratory.Fetch.Extensions;
 using Labradoratory.Fetch.Processors;
+using Labradoratory.Fetch.Processors.DataPackages;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Xunit;
@@ -35,6 +39,64 @@ namespace Labradoratory.Fetch.Test.Extensions
             TestServiceAdded<ProcessorPipeline>();
         }
 
+        [Fact]
+        public void AddFetchAddingProcessor_Success()
+        {
+            TestProcessorAdd<EntityAddingPackage<TestEntity>>(sc => sc.AddFetchAddingProcessor<TestEntity, TestProcessor>());
+        }
+
+        [Fact]
+        public void AddFetchAddedProcessor_Success()
+        {
+            TestProcessorAdd<EntityAddedPackage<TestEntity>>(sc => sc.AddFetchAddedProcessor<TestEntity, TestProcessor>());
+        }
+
+        [Fact]
+        public void AddFetchUpdatingProcessor_Success()
+        {
+            TestProcessorAdd<EntityUpdatingPackage<TestEntity>>(sc => sc.AddFetchUpdatingProcessor<TestEntity, TestProcessor>());
+        }
+
+        [Fact]
+        public void AddFetchUpdateProcessor_Success()
+        {
+            TestProcessorAdd<EntityUpdatedPackage<TestEntity>>(sc => sc.AddFetchUpdatedProcessor<TestEntity, TestProcessor>());
+        }
+
+        [Fact]
+        public void AddFetchDeletingProcessor_Success()
+        {
+            TestProcessorAdd<EntityDeletingPackage<TestEntity>>(sc => sc.AddFetchDeletingProcessor<TestEntity, TestProcessor>());
+        }
+
+        [Fact]
+        public void AddFetchDeletedProcessor_Success()
+        {
+            TestProcessorAdd<EntityDeletedPackage<TestEntity>>(sc => sc.AddFetchDeletedProcessor<TestEntity, TestProcessor>());
+        }
+
+        private void TestProcessorAdd<TPackage>(Func<IServiceCollection, IServiceCollection> test)
+            where TPackage : DataPackage
+        {
+            var descriptors = new List<ServiceDescriptor>();
+            var mockServiceCollection = new Mock<IServiceCollection>(MockBehavior.Strict);
+            mockServiceCollection
+                .Setup(sc => sc.GetEnumerator())
+                .Returns(Enumerable.Empty<ServiceDescriptor>().GetEnumerator());
+            mockServiceCollection
+                .Setup(sc => sc.Add(It.IsAny<ServiceDescriptor>()))
+                .Callback<ServiceDescriptor>(d => descriptors.Add(d));
+
+            var expectedServiceCollection = mockServiceCollection.Object;
+            var result = test(expectedServiceCollection);
+            Assert.Same(expectedServiceCollection, result);
+
+            mockServiceCollection.Verify(sc => sc.Add(
+                It.Is<ServiceDescriptor>(v => v.ServiceType == typeof(IProcessor<TPackage>)
+                && v.ImplementationType == typeof(TestProcessor))),
+                Times.Once());
+        }
+
         private void TestServiceAdded<TServiceAndImplementation>()
         {
             TestServiceAdded<TServiceAndImplementation, TServiceAndImplementation>();
@@ -60,5 +122,65 @@ namespace Labradoratory.Fetch.Test.Extensions
             Assert.Equal(typeof(TImplementation), descriptor.ImplementationType);
             Assert.Equal(ServiceLifetime.Singleton, descriptor.Lifetime);
         }
+
+        public class TestEntity : Entity
+        {
+            public override object[] DecodeKeys(string encodedKeys)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public override string EncodeKeys()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public override object[] GetKeys()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public class TestProcessor :
+            IProcessor<EntityAddedPackage<TestEntity>>,
+            IProcessor<EntityAddingPackage<TestEntity>>,
+            IProcessor<EntityUpdatedPackage<TestEntity>>,
+            IProcessor<EntityUpdatingPackage<TestEntity>>,
+            IProcessor<EntityDeletedPackage<TestEntity>>,
+            IProcessor<EntityDeletingPackage<TestEntity>>
+        {
+            public uint Priority => throw new System.NotImplementedException();
+
+            public Task ProcessAsync(EntityAddedPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Task ProcessAsync(EntityAddingPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Task ProcessAsync(EntityUpdatedPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Task ProcessAsync(EntityUpdatingPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Task ProcessAsync(EntityDeletedPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public Task ProcessAsync(EntityDeletingPackage<TestEntity> package, CancellationToken cancellationToken = default)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
     }
 }
