f4530377f0d95a3ce200835aaaefe2254791254c
Ed Downs <ed@labradoratory.com>
2020-02-02 20:37:58 -0800

    Adds some documentation

--- src/Labradoratory.Fetch/ChangeTracking/ChangePath.cs
+++ src/Labradoratory.Fetch/ChangeTracking/ChangePath.cs
@@ -5,15 +5,32 @@ using System.Text.RegularExpressions;
 
 namespace Labradoratory.Fetch.ChangeTracking
 {
+    /// <summary>
+    /// Represents an immutible path to a change in object hierarchy.
+    /// </summary>
     public class ChangePath
     {
+        /// <summary>
+        /// Represents an empty path.
+        /// </summary>
         public static readonly ChangePath Empty = new ChangePath();
 
+        /// <summary>
+        /// Creates a <see cref="ChangePath"/> from the provided part.
+        /// </summary>
+        /// <param name="part">The part to create the path from.</param>
+        /// <returns></returns>
         public static ChangePath Create(IChangePathPart part)
         {
             return Empty.Append(part);
         }
 
+        /// <summary>
+        /// Creates a <see cref="ChangePath"/> using the provided property name to create 
+        /// the initial <see cref="ChangePathProperty"/>.
+        /// </summary>
+        /// <param name="property">The property.</param>
+        /// <returns></returns>
         public static ChangePath Create(string property)
         {
             return Empty.AppendProperty(property);
@@ -47,43 +64,86 @@ namespace Labradoratory.Fetch.ChangeTracking
         /// </summary>
         public ChangeTarget Target { get; }
 
+        /// <summary>
+        /// Gets the parts that make up the path.
+        /// </summary>
         public IReadOnlyList<IChangePathPart> Parts { get; }
 
+        /// <summary>
+        /// Appends the part to the end, creating a new <see cref="ChangePath"/>.
+        /// </summary>
+        /// <param name="part">The part to append.</param>
+        /// <returns>A new <see cref="ChangePath"/> with the part appended.</returns>
         public ChangePath Append(IChangePathPart part)
         {
             return new ChangePath(Parts, part, Target);
         }
 
+        /// <summary>
+        /// Appends the provided property name, creating a new <see cref="ChangePath"/>.
+        /// </summary>
+        /// <param name="property">The property to append.</param>
+        /// <returns>A new <see cref="ChangePath"/> with the <see cref="ChangePathProperty"/> appended.</returns>
         public ChangePath AppendProperty(string property)
         {
             return Append(new ChangePathProperty(property));
         }
 
+        /// <summary>
+        /// Appends the provided index, creating a new <see cref="ChangePath"/>.
+        /// </summary>
+        /// <param name="index">The index to append.</param>
+        /// <returns>A new <see cref="ChangePath"/> with the <see cref="ChangePathIndex"/> appended.</returns>
         public ChangePath AppendIndex(string index)
         {
             return Append(new ChangePathIndex(index));
         }
 
+        /// <summary>
+        /// Appends the provided key, creating a new <see cref="ChangePath"/>.
+        /// </summary>
+        /// <param name="key">The key to append.</param>
+        /// <returns>A new <see cref="ChangePath"/> with the <see cref="ChangePathKey"/> appended.</returns>
         public ChangePath AppendKey(object key)
         {
             return Append(new ChangePathKey(key));
         }
 
+        /// <summary>
+        /// Creates a new <see cref="ChangePath"/> with the specified target.
+        /// </summary>
+        /// <param name="target">The target of the path.</param>
+        /// <returns>A new <see cref="ChangePath"/> with the specified target.</returns>
         public ChangePath WithTarget(ChangeTarget target)
         {
             return new ChangePath(Parts, target);
         }
 
+        /// <summary>
+        /// Converts the <see cref="ChangePath"/> to its string representation.
+        /// </summary>
+        /// <remarks>The '.' is used to separate the parts.</remarks>
         public override string ToString()
         {
             return ToString('.');
         }
 
+        /// <summary>
+        /// Converts the <see cref="ChangePath"/> to its string representation,
+        /// using the provided charater to separate the parts.
+        /// </summary>
         public string ToString(char separator)
         {
             return string.Join(separator, Parts);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (!(obj is ChangePath ck))
@@ -92,31 +152,59 @@ namespace Labradoratory.Fetch.ChangeTracking
             return Parts.SequenceEqual(ck.Parts);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return ToString().GetHashCode();
         }
     }
 
+    /// <summary>
+    /// A marker interface, identifying a part of a <see cref="ChangePath"/>.
+    /// </summary>
     public interface IChangePathPart
-    {
+    {}
 
-    }
-
+    /// <summary>
+    /// Represents a property in a <see cref="ChangePath"/>.
+    /// </summary>
+    /// <seealso cref="Labradoratory.Fetch.ChangeTracking.IChangePathPart" />
     public class ChangePathProperty : IChangePathPart
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangePathProperty"/> class.
+        /// </summary>
+        /// <param name="property">The property.</param>
         public ChangePathProperty(string property)
         {
             Property = property;
         }
 
+        /// <summary>
+        /// Gets the property.
+        /// </summary>
         public string Property { get; }
 
+        /// <summary>
+        /// Converts the part to its string representation.
+        /// </summary>
         public override string ToString()
         {
             return Property;
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (!(obj is ChangePathProperty cpp))
@@ -125,16 +213,34 @@ namespace Labradoratory.Fetch.ChangeTracking
             return Property == cpp.Property;
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return $"Property{Property}".GetHashCode();
         }
     }
 
+    /// <summary>
+    /// Represents an index (Array, List or other) in a <see cref="ChangePath"/>.
+    /// </summary>
+    /// <seealso cref="Labradoratory.Fetch.ChangeTracking.IChangePathPart" />
     public class ChangePathIndex : IChangePathPart
     {
+        /// <summary>
+        /// The regular expression that an index value must match.
+        /// </summary>
         public static readonly Regex IndexRegex = new Regex(@"^(\d*|-)$", RegexOptions.Compiled);
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangePathIndex"/> class.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <exception cref="ArgumentException">index - Index value must be a positive integer or '-'.</exception>
         public ChangePathIndex(string index)
         {
             if (!IndexRegex.IsMatch(index))
@@ -143,13 +249,26 @@ namespace Labradoratory.Fetch.ChangeTracking
             Index = index;
         }
 
+        /// <summary>
+        /// Gets the index.
+        /// </summary>
         public string Index { get; }
 
+        /// <summary>
+        /// Converts the part to its string representation.
+        /// </summary>
         public override string ToString()
         {
             return Index;
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (!(obj is ChangePathIndex cpi))
@@ -158,26 +277,53 @@ namespace Labradoratory.Fetch.ChangeTracking
             return Index == cpi.Index;
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return $"Index{Index}".GetHashCode();
         }
     }
 
+    /// <summary>
+    /// Represents a key (Dictionary or other) in a <see cref="ChangePath"/>.
+    /// </summary>
+    /// <seealso cref="Labradoratory.Fetch.ChangeTracking.IChangePathPart" />
     public class ChangePathKey : IChangePathPart
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangePathKey"/> class.
+        /// </summary>
+        /// <param name="key">The key.</param>
         public ChangePathKey(object key)
         {
             Key = key.ToString();
         }
 
+        /// <summary>
+        /// Gets the key.
+        /// </summary>
         public string Key { get; }
 
+        /// <summary>
+        /// Converts the part to its string representation.
+        /// </summary>
         public override string ToString()
         {
             return Key;
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (!(obj is ChangePathKey cpk))
@@ -186,6 +332,12 @@ namespace Labradoratory.Fetch.ChangeTracking
             return Key == cpk.Key;
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return $"Key{Key}".GetHashCode();
